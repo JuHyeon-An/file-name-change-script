719ee4ec227d4900c8011b5de0213871dfc16ba4
JuHyeon-An <ajh2042@naver.com>
2022-06-28 17:49:23 +0900

    [FEAT] Add fileMap file

---
+++ fileMap.js
@@ -0,0 +1,98 @@
+export const fileMap = {
+    pub: {
+        1: {
+            smt: "terms03.pdf",
+            wooriib: "2132.pdf",
+            smtweb: "product_15.gif",
+        },
+        2: {
+            smt: "instruction01.pdf",
+            wooriib: "2131.pdf",
+            smtweb: "product_15_2.gif",
+        },
+    },
+    cma: {
+        1: {
+            smt: "terms12.pdf",
+            wooriib: "2122.pdf",
+            smtweb: "product_14.gif",
+        },
+        2: {
+            smt: "instruction07.pdf",
+            wooriib: "21111.pdf",
+            smtweb: "product_14_2.gif",
+        },
+    },
+    cmanote: {
+        1: {
+            smt: "terms11.pdf",
+            wooriib: "21112.pdf",
+            smtweb: "product_21.gif",
+        },
+        2: {
+            smt: "instruction06.pdf",
+            wooriib: "2121.pdf",
+            smtweb: "product_21_2.gif",
+        },
+    },
+    loan: {
+        1: {
+            smt: "terms22.pdf",
+            wooriib: "37.pdf",
+            smtweb: "product_33.gif",
+        },
+        2: {
+            smt: "instruction08.pdf",
+            wooriib: "21241.pdf",
+            smtweb: "product_33_2.gif",
+        },
+    },
+    tax: {
+        1: {
+            smt: "none.pdf",
+            wooriib: "37.pdf",
+            smtweb: "product_22.gif",
+        },
+        2: {
+            smt: "none.pdf",
+            wooriib: "21131.pdf",
+            smtweb: "product_22_2.gif",
+        },
+    },
+    dream3: {
+        1: {
+            smt: "terms07.pdf",
+            wooriib: "21232.pdf",
+            smtweb: "product_32.gif",
+        },
+        2: {
+            smt: "instruction03.pdf",
+            wooriib: "21231.pdf",
+            smtweb: "product_32_2.gif",
+        },
+    },
+    joeun: {
+        1: {
+            smt: "terms04.pdf",
+            wooriib: "21152.pdf",
+            smtweb: "product_24.gif",
+        },
+        2: {
+            smt: "instruction02.pdf",
+            wooriib: "21181.pdf",
+            smtweb: "product_24_2.gif",
+        },
+    },
+    hi3: {
+        1: {
+            smt: "terms26.pdf",
+            wooriib: "42.pdf",
+            smtweb: "product_42.gif",
+        },
+        2: {
+            smt: "instruction15.pdf",
+            wooriib: "42_2.pdf",
+            smtweb: "product_42_2.gif",
+        },
+    },
+};
